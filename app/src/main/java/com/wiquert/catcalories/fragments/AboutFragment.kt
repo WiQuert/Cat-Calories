7f78169f59e52cfa7e475660a7bb0719bb5c0f1f
WiQuert <roshev@slaps.ru>
2024-03-16 12:08:25 +0300

    Initializing new fragments

---
+++ app/src/main/java/com/wiquert/catcalories/fragments/AboutFragment.kt
@@ -0,0 +1,25 @@
+package com.wiquert.catcalories.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wiquert.catcalories.R
+
+class AboutFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = AboutFragment()
+    }
+}
