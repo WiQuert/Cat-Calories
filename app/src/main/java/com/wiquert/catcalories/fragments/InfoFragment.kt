a899baa53ce77b16844c9dff443d6e00c13b96c2
WiQuert <roshev@slaps.ru>
2024-03-18 19:08:16 +0300

    Creating info fragment

--- app/src/main/java/com/wiquert/catcalories/fragments/InfoFragment.kt
+++ app/src/main/java/com/wiquert/catcalories/fragments/InfoFragment.kt
@@ -6,21 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.wiquert.catcalories.R
+import com.wiquert.catcalories.databinding.FragmentInfoBinding
 
 
 class InfoFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        binding = FragmentInfoBinding.inflate(inflater, container, false)
+        return binding.root
     }
-
-    companion object {
-             @JvmStatic
-        fun newInstance() = InfoFragment()
-            }
-    }
+}
