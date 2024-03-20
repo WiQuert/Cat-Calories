5ffe6da335d091a519233415996e6ee29795c40e
WiQuert <roshev@slaps.ru>
2024-03-20 16:21:31 +0300

    Finished About fragment

--- app/src/main/java/com/wiquert/catcalories/fragments/AboutFragment.kt
+++ app/src/main/java/com/wiquert/catcalories/fragments/AboutFragment.kt
@@ -1,25 +1,64 @@
 package com.wiquert.catcalories.fragments
 
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.wiquert.catcalories.BuildConfig
 import com.wiquert.catcalories.R
+import com.wiquert.catcalories.databinding.FragmentAboutBinding
+
 
 class AboutFragment : Fragment() {
 
+    private lateinit var binding: FragmentAboutBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_about, container, false)
+    ): View {
+        binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        @JvmStatic
-        fun newInstance() = AboutFragment()
+        binding.tvAppName.text = getString(R.string.app_name)
+        binding.tvAuthor.text = getText(R.string.developed)
+
+        setAnimation(R.drawable.catsplash)
+
+        setVersionApp()
+
     }
+
+
+
+    private fun setAnimation(image: Int) {
+        Thread{
+            val source = ImageDecoder.createSource(resources, image)
+            val drawable = ImageDecoder.decodeDrawable(source)
+            binding.imAnimation.post {
+                binding.imAnimation.setImageDrawable(drawable)
+                if (drawable is AnimatedImageDrawable) {
+                    drawable.start()
+                }
+            }
+        } .start()
+    }
+
+
+    private fun setVersionApp() = with(binding) {
+        val versionHeader = getString(R.string.version_app)
+        val versionNum = BuildConfig.VERSION_NAME
+        val versionApp = versionHeader + versionNum
+        tvVersionApp.text = versionApp
+    }
+
+
+
 }
